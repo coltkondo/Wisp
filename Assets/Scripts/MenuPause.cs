f6397857a2eb601bddb5733a3ed184d0090d0bbf
j-fangwu <johnfangwu@gmail.com>
2024-03-21 03:17:57 -0400

    Added Pause Menu, fixed Movement Jittering, adjusted dialogue UI

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+                GameIsPaused = false;
+            } else {
+                Pause();
+                GameIsPaused = true;
+            }
+        }
+    }
+
+    public void Resume() {
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Pause() {
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void Menu() {
+        SceneManager.LoadScene("GameStart");
+    }
+}
