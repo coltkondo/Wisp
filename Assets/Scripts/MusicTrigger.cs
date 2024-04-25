8f80c53da31a308e79f95f39180e2443cb0bfa87
Diobeato <157068436+Diobeato@users.noreply.github.com>
2024-04-24 21:11:17 -0400

    Tutorial Music Fix + Scroll Typos

--- Assets/Scripts/MusicTrigger.cs
+++ Assets/Scripts/MusicTrigger.cs
@@ -8,24 +8,56 @@ public class MusicTrigger : MonoBehaviour
     public AudioSource audioSourceB; // Assign through the inspector
     public float fadeTime = 1.5f;    // Duration of the fade in seconds
 
+    public float triggerCooldown;
+
+    private bool onCooldown = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) // Make sure the player has a tag "Player"
         {
-            // Determine the direction of the player
-            if (transform.position.x < other.transform.position.x)
+            // Player is moving from B to A
+            if (audioSourceA.isPlaying == false)
             {
-                // Player is moving from A to B
-                StartCoroutine(FadeOut(audioSourceA, fadeTime));
-                StartCoroutine(FadeIn(audioSourceB, fadeTime));
-            }
-            else
-            {
-                // Player is moving from B to A
+                Debug.Log("Playing Track A");
                 StartCoroutine(FadeOut(audioSourceB, fadeTime));
                 StartCoroutine(FadeIn(audioSourceA, fadeTime));
             }
+            
+
+            /*Debug.Log("Cooldown Status: " + onCooldown + "");
+            if (onCooldown == false)
+            {
+                // Determine the direction of the player
+                if (transform.position.x < other.transform.position.x)
+                {
+                    // Player is moving from A to B
+                    onCooldown = true;
+                    Debug.Log("Playing Track B");
+                    StartCoroutine(FadeOut(audioSourceA, fadeTime));
+                    StartCoroutine(FadeIn(audioSourceB, fadeTime));
+                    StartCoroutine(TriggerCD());
+
+                }
+                else
+                {
+                    // Player is moving from B to A
+                    onCooldown = true;
+                    Debug.Log("Playing Track A");
+                    StartCoroutine(FadeOut(audioSourceB, fadeTime));
+                    StartCoroutine(FadeIn(audioSourceA, fadeTime));
+                    StartCoroutine(TriggerCD());
+                }
+            }*/
+            
         }
+    }
+
+    private IEnumerator TriggerCD()
+    {
+        onCooldown = true;
+        yield return new WaitForSeconds(triggerCooldown);
+        onCooldown = false;
     }
 
      private IEnumerator FadeIn(AudioSource audioSource, float duration)
@@ -39,6 +71,7 @@ public class MusicTrigger : MonoBehaviour
             audioSource.volume = Mathf.Lerp(0, 0.3f, currentTime / duration);
             yield return null;
         }
+        //gameObject.SetActive(false);
     }
 
     private IEnumerator FadeOut(AudioSource audioSource, float duration)
